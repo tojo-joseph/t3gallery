2bd52c24a9acdcb6729fd8122b483ee8731b9796
tojo-joseph <tojo.joseph18@gmail.com>
2024-08-09 08:49:42 +0530

    root layout and navbar changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/450c09aa-dd7c-47c2-9bb2-377456fe64c8-y81kse.png",
+  "https://utfs.io/f/978cef29-f432-43fd-a93a-59fa3b5c72d7-rrks80.png",
+  "https://utfs.io/f/45291020-518c-49e2-9e8e-a71402c0d6ae-cm0wr3.png",
+  "https://utfs.io/f/a3f7d160-8219-4665-96ba-c71b6cee0982-n03ws.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello World!
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
